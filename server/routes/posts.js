c25b50e3687793bfe5d56f87806459d2fdff1a94
dibkb <dibas9110@gmail.con>
2022-11-06 12:56:11 +0530

    rename files

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPosts, createPosts } from "../controller/posts.js";
+import { getPosts, createPosts } from "../controller/postsController.js";
 export const postRouter = express.Router();
 postRouter.get("/", getPosts);
 postRouter.post("/", createPosts);
