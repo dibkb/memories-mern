3b8b618d7ac9bf6e6378ec42a38246fd442b53af
dibkb <dibas9110@gmail.con>
2022-11-06 13:10:44 +0530

    changed mongo connection to async

--- server/index.js
+++ server/index.js
@@ -5,22 +5,22 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import { postRouter } from "./routes/posts.js";
 dotenv.config();
-const mongoString = process.env.MONG0_URL;
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-database.on("error", (error) => {
-  console.log(error);
-});
-database.once("connected", () => {
-  console.log("Database Connected");
-});
 const app = express();
+// middlewares
 app.use(cors());
 app.use(bodyParser.json({ limit: `30mb`, extended: true }));
 app.use(bodyParser.urlencoded({ limit: `30mb`, extended: true }));
 app.use(express.json());
-// post routes
+// routes
 app.use("/posts", postRouter);
-app.listen(4000, () => {
-  console.log(`server running on PORT 4000`);
-});
+mongoose
+  .connect(process.env.MONG0_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
