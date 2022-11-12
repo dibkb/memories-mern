66cc0ceffc0a1534912786e9c288c91df604a234
dibkb <dibas9110@gmail.con>
2022-11-12 18:59:26 +0530

    form UI complete

--- client/src/components/Form Components/RegisterForm.tsx
+++ client/src/components/Form Components/RegisterForm.tsx
@@ -6,11 +6,17 @@ import {
   EyeIcon,
   EyeSlashIcon,
 } from "@heroicons/react/24/solid";
+import { TogleOff, TogleOn } from "../../utils/Icons";
+import CreateAccount from "../CreateAccount";
+import { Link } from "react-router-dom";
 const RegisterForm: React.FC = () => {
   const [showPw, setShowPw] = useState<boolean>(false);
   const [showConfirmPw, setShowConfirmPw] = useState<boolean>(false);
+  const [acceptTerms, setAcceptterms] = useState<boolean>(false);
+  const [isValidated, setIsValidated] = useState<boolean>(false);
   return (
     <section className={styles["__form"]}>
+      {/* Profile Picture */}
       <div className={styles["profile__input"]}>
         <div className={styles["icon__container"]}>
           <UserCircleIcon className={styles["user__icon"]} />
@@ -18,6 +24,7 @@ const RegisterForm: React.FC = () => {
         </div>
         <p>Choose Profile Picture</p>
       </div>
+      {/* Name*/}
       <div className={styles["name"]}>
         <span className={styles["__first"]}>
           <pre className={styles["__label"]}>First Name</pre>
@@ -28,10 +35,12 @@ const RegisterForm: React.FC = () => {
           <input type="text" />
         </span>
       </div>
+      {/* Email*/}
       <div className={styles["email"]}>
         <pre className={styles["__label"]}>Email address</pre>
         <input type="text" />
       </div>
+      {/* Password*/}
       <div className={styles["password"]}>
         <pre className={styles["__label"]}>Password</pre>
         <div className={styles["input__container"]}>
@@ -45,6 +54,7 @@ const RegisterForm: React.FC = () => {
           </span>
         </div>
       </div>
+      {/* Confirm-Password*/}
       <div className={styles["confirm__password"]}>
         <pre className={styles["__label"]}>Confirm Password</pre>
         <div className={styles["input__container"]}>
@@ -58,6 +68,35 @@ const RegisterForm: React.FC = () => {
           </span>
         </div>
       </div>
+      {/* Terms & Conditions*/}
+      <div
+        className={styles["terms"]}
+        onClick={() => {
+          setAcceptterms((prev) => !prev);
+        }}
+      >
+        <span className={styles["__switchContainer"]}>
+          {acceptTerms && <TogleOn />}
+          {!acceptTerms && <TogleOff />}
+        </span>
+        <p className={styles[`__text-${acceptTerms}`]}>
+          I accept the Terms of Service as well as Privacy Policy
+        </p>
+      </div>
+      {/* Create button */}
+      <button
+        className={styles[`create__btn__${acceptTerms}`]}
+        disabled={!acceptTerms}
+      >
+        CREATE ACCOUNT
+      </button>
+      {/* Already have an account? */}
+      <Link to="/login" className="mx-auto">
+        <a className={styles["__login"]}>
+          <p>Already have an account </p>
+          <p className={styles["neon"]}>Log In</p>
+        </a>
+      </Link>
     </section>
   );
 };
