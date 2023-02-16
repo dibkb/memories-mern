457526a5348a74e123eb07d31f8ebbc2afc55bd5
dibkb <dibas9110@gmail.con>
2023-02-16 23:30:47 +0530

    Profile details in header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -3,10 +3,20 @@ import styles from "../src/styles/Memories.module.scss";
 import Logo from "./components/Logo";
 import { colors } from "./utils/colors";
 import { UserContext, userInfo } from "./UserContext";
+import ProfileInfo from "./components/ProfileInfo";
 const Header: React.FC<Header> = ({ user }) => {
   return (
     <nav className={styles["nav"]}>
       <Logo large={false} color={colors.logo} />
+      <span
+        style={{
+          display: "flex",
+          columnGap: "2rem",
+        }}
+      >
+        <ProfileInfo name={user?.firstName} image={user?.image} />
+        <button className={styles["logout__btn"]}>Logout</button>
+      </span>
     </nav>
   );
 };
---
+++ client/src/components/ProfileInfo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { colors } from "../utils/colors";
+const ProfileInfo: React.FC<ProfileInfo> = ({ name, image }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        columnGap: "1rem",
+      }}
+    >
+      <img
+        src={image}
+        alt=""
+        style={{
+          width: "36px",
+          height: "36px",
+          borderRadius: "50%",
+          border: `2px solid ${colors.neon}`,
+        }}
+      />
+      <small
+        style={{
+          fontSize: "16px",
+          color: colors.textSsecondary,
+        }}
+      >
+        {name}
+      </small>
+    </div>
+  );
+};
+
+export default ProfileInfo;
+interface ProfileInfo {
+  name: string | undefined;
+  image: string | undefined;
+}
