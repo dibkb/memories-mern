a0a9e097d38cc399ebc5b9eb598bf2645e241463
dibkb <dibas9110@gmail.con>
2023-02-18 10:29:34 +0530

    create post display blob

--- client/src/components/CreatePostModal.tsx
+++ client/src/components/CreatePostModal.tsx
@@ -1,6 +1,7 @@
-import React, { ReactComponentElement } from "react";
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import styles from "../styles/Postmodal.module.scss";
+import FileBase64 from "react-file-base64";
 export const CreatePostModal: React.FC<CreatePostModal> = ({
   setShowModal,
 }) => {
@@ -8,6 +9,22 @@ export const CreatePostModal: React.FC<CreatePostModal> = ({
     e.target.style.height = "inherit";
     e.target.style.height = `${e.target.scrollHeight}px`;
   };
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [file, setFile] = useState<any>(null);
+  const [displayPicture, setDisplayPicture] = useState<string>("");
+  const fileSelectorHandler = (base64: any) => {
+    setFile(base64);
+  };
+  useEffect(() => {
+    let blobURL: string;
+    if (file)
+      fetch(file.base64)
+        .then((res) => res.blob())
+        .then((blob) => (blobURL = URL.createObjectURL(blob)))
+        .then((img) => setDisplayPicture(img));
+    return () => URL.revokeObjectURL(blobURL);
+  }, [file]);
   return createPortal(
     <div className={styles["modal__overlay"]}>
       <div className={styles["container"]}>
@@ -17,14 +34,30 @@ export const CreatePostModal: React.FC<CreatePostModal> = ({
         >
           Close
         </button>
-        <input type="text" placeholder="Title" className={styles["title"]} />
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+          value={title}
+          className={styles["title"]}
+        />
         <textarea
           placeholder="Description..."
           rows={4}
           cols={50}
           className={styles["textarea"]}
           onKeyDown={handleKeyDown}
+          value={description}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+            setDescription(e.target.value)
+          }
         ></textarea>
+        <div className={styles["file__input"]}>
+          <FileBase64 multiple={false} onDone={fileSelectorHandler} />
+        </div>
+        <img src={displayPicture} className={styles["image__preview"]} />
       </div>
     </div>,
     document.body
