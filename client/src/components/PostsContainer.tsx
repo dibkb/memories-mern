c4f97e651f68a6ab868a5b3cc1566c57ed2a01e3
dibkb <dibas9110@gmail.con>
2023-02-21 16:44:09 +0530

    asthetic changes

--- client/src/components/PostsContainer.tsx
+++ client/src/components/PostsContainer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // ------------styles-----------------
 import styles from "../styles/Postcontainer.module.scss";
+import { colors } from "../utils/colors";
 const PostsContainer: React.FC<PostsContainer> = ({ posts }) => {
   const content = posts.map((post: any) => {
     return (
@@ -13,7 +14,31 @@ const PostsContainer: React.FC<PostsContainer> = ({ posts }) => {
           backgroundPosition: "center",
           backgroundSize: "cover",
         }}
-      ></div>
+      >
+        <div className={styles.creator__info}>
+          <img
+            src={post.creatorImage}
+            alt=""
+            style={{
+              width: "36px",
+              height: "36px",
+              borderRadius: "50%",
+              border: `2px solid ${colors.neon}`,
+            }}
+          />
+          <small
+            style={{
+              fontSize: "16px",
+              color: colors.textSsecondary,
+            }}
+            className={styles["creator__name"]}
+          >
+            {post.creatorName}
+          </small>
+        </div>
+        <small className={styles["post__title"]}>{post.title}</small>
+        <p className={styles["description"]}>{post.message}</p>
+      </div>
     );
   });
   return <div className={styles.container__styles}>{content}</div>;
