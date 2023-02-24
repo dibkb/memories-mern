cc32fcc83578c8871dbf80c55729fcf260c7fbcb
Dibas K Borborah <59993782+dibkb@users.noreply.github.com>
2023-02-24 21:03:19 +0530

    Delete client directory

--- client/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "../features/posts/postSlice";
-import userReducer from "../features/userPosts/userPostsSlice";
-export const store = configureStore({
-  reducer: {
-    posts: postReducer,
-    user: userReducer,
-  },
-});
