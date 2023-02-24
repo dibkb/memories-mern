b540b1a68f1f7fba136a5c38147e68313764647e
dibkb <dibas9110@gmail.con>
2023-02-24 21:40:43 +0530

    api working

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,1 +1,2 @@
-export const BASEURL = "http://localhost:4000" as string;
+// export const BASEURL = "http://localhost:4000" as string;
+export const BASEURL = "https://memoriesapi.onrender.com" as string;
